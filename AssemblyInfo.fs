3974349381ef143085de71b4c2ee34a79557e238
ipavl <ipavl@users.sourceforge.net>
2014-10-13 22:53:19 -0400

    Initial commit; finds .md files and converts them to .html

---
+++ AssemblyInfo.fs
@@ -0,0 +1,18 @@
+﻿module fssg.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly:AssemblyTitle("fssg")>]
+[<assembly:AssemblyDescription("Static Site Generator in F#")>]
+[<assembly:AssemblyConfiguration("")>]
+[<assembly:AssemblyCompany("")>]
+[<assembly:AssemblyProduct("")>]
+[<assembly:AssemblyCopyright("ipavl")>]
+[<assembly:AssemblyTrademark("")>]
+[<assembly:AssemblyVersion("1.0.0.0")>]
+()
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
---
+++ Program.fs
@@ -0,0 +1,33 @@
+﻿(*
+  Untitled static site generator in F#
+  Author: ipavl <https://github.com/ipavl>
+  Date: October 13, 2014
+ *)
+
+open FSharp.Markdown
+
+open System
+open System.IO
+
+/// Functions that are used to parse files (e.g. Markdown, templates).
+module Parse =
+    /// Parses and converts Markdown files into HTML files.
+    let markdown() =
+        for mdFile in Directory.EnumerateFiles(Environment.CurrentDirectory, "*.md", SearchOption.AllDirectories) do
+            let htmlFile = Path.ChangeExtension(mdFile, "html")
+            let html = Markdown.TransformHtml(File.ReadAllText mdFile)
+
+            File.WriteAllText(htmlFile, html)
+
+            printfn "%s -> %s" mdFile htmlFile
+
+
+[<EntryPoint>]
+let main argv = 
+    //printfn "%A" argv
+    printfn "F# Static Site Generator v0.1"
+    printfn "Current working directory: %s" Environment.CurrentDirectory 
+
+    Parse.markdown()
+
+    0 // return an integer exit code
