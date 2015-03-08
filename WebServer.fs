c15ced0aeeb69e8a99a31ccefec5ea221b36d8f0
ipavl <ipavl@users.sourceforge.net>
2015-03-08 12:59:13 -0400

    Fix file not found exception with the default route

--- WebServer.fs
+++ WebServer.fs
@@ -26,12 +26,13 @@ module WebServer
         } |> Async.Start
 
     let routeHandler (req : HttpListenerRequest, webRoot : String) =
+        let indexFile = webRoot + "/index.html"
         let file = Path.Combine(webRoot, Uri(httpServer).MakeRelativeUri(req.Url).OriginalString)
         printfn "Requested: '%s'" file
 
-        if (file.Equals(webRoot)) then
+        if (file.Equals(webRoot) && File.Exists(indexFile)) then
             // If the file path is the web root, return the default index file
-            File.ReadAllText(webRoot + "/index.html")
+            File.ReadAllText(indexFile)
         else if (File.Exists file) then
             // Return the requested file
             File.ReadAllText(file)
