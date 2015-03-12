6759273606f9d1394d90cd64831f5aa18a3ea8d2
ipavl <ipavl@users.sourceforge.net>
2015-03-12 00:23:08 -0400

    Fix homepage when no path is specified

--- WebServer.fs
+++ WebServer.fs
@@ -32,14 +32,14 @@ module WebServer
 
     /// Handles page routing requests.
     let routeHandler (req : HttpListenerRequest, webRoot : String) =
-        let indexFile = webRoot + indexFile
+        let homePage = webRoot + "/" + indexFile
         let file = Path.Combine(webRoot, Uri(listenAddress).MakeRelativeUri(req.Url).OriginalString)
 
         printfn "Requested: '%s'" file
 
-        if (file.Equals(webRoot) && File.Exists(indexFile)) then
+        if (file.Equals(webRoot) && File.Exists(homePage)) then
             // If the file path is the web root, return the default index file
-            File.ReadAllText(indexFile)
+            File.ReadAllText(homePage)
         else if (File.Exists file) then
             // Return the requested file
             File.ReadAllText(file)
